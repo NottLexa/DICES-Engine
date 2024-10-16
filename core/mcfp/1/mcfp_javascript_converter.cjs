d1c5c82ab9ad09cdcd85c2bebd798642a9eac95d
NotLexa <94920046+NottLexa@users.noreply.github.com>
2024-10-16 18:35:33 +0300

    DVLP [22]: converted effects now store `use_dices` key 1) mcfp/1/mjc: convert_effect's return_data now have `use_dices` key 2) mcfp/1/mjc: convert_formula returns `use_dices` as the 3rd returned value. `use_dices` is true if any dice is used in the formula.

--- core/mcfp/1/mcfp_javascript_converter.cjs
+++ core/mcfp/1/mcfp_javascript_converter.cjs
@@ -19,14 +19,15 @@ const mjc = function(mfb) {
     };
 
     this.convert_effect = function(target_attribute, formula_block, effect_type, target_attribute_property = 'value') {
-        let return_data = {function: null, dependencies: new Set(), target_attribute: target_attribute, target_property: target_attribute_property};
-        let [converted_formula, attribute_dependencies] = mjc_this.convert_formula(formula_block);
+        let return_data = {function: null, dependencies: new Set(), target_attribute: target_attribute, target_property: target_attribute_property, use_dices: false};
+        let [converted_formula, attribute_dependencies, use_dices] = mjc_this.convert_formula(formula_block);
         if (converted_formula !== null) {
-            return_data.function = new Function('attributes', 'functions', 'self_attribute', 'dices_iterator',
+            return_data.function = new Function('attributes', 'functions', 'self_attribute', ...(use_dices ? ['dices_iterator'] : []),
                 effect_type === '.='
                 ? `attributes["${target_attribute}"]._${target_attribute_property}.push(...${converted_formula})`
                 : `attributes["${target_attribute}"]._${target_attribute_property} ${effect_type} `+converted_formula);
             return_data.dependencies = attribute_dependencies;
+            return_data.use_dices = use_dices;
         }
         return return_data;
     }
@@ -34,6 +35,7 @@ const mjc = function(mfb) {
     this.convert_formula = function(formula_block) {
         let converted_formula = '';
         let attribute_dependencies = new Set();
+        let use_dices = false;
         switch (formula_block.constructor) {
             case mfb.ConstantValue:
                 if (typeof formula_block.value === 'string') {
@@ -57,11 +59,10 @@ const mjc = function(mfb) {
             case mfb.Function:
                 let converted_args = [];
                 for (let arg of formula_block.args) {
-                    let [converted_argument, argument_attribute_dependencies] = this.convert_formula(arg);
+                    let [converted_argument, argument_attribute_dependencies, argument_uses_dices] = this.convert_formula(arg);
                     if (converted_argument === null) return [null, []];
-                    for (let dependency of argument_attribute_dependencies) {
-                        attribute_dependencies.add(dependency);
-                    }
+                    for (let dependency of argument_attribute_dependencies) attribute_dependencies.add(dependency);
+                    use_dices ||= argument_uses_dices;
                     converted_args.push(converted_argument);
                 }
                 if (mjc_this.math_operators.hasOwnProperty(formula_block.function) && false) {
@@ -78,25 +79,28 @@ const mjc = function(mfb) {
             case mfb.Array:
                 let converted_elements = [];
                 for (let element of formula_block.array) {
-                    let [converted_argument, argument_attribute_dependencies] = this.convert_formula(element);
+                    let [converted_argument, argument_attribute_dependencies, argument_uses_dices] = this.convert_formula(element);
                     if (converted_argument === null) return [null, []];
                     for (let dependency of argument_attribute_dependencies) attribute_dependencies.add(dependency);
+                    use_dices ||= argument_uses_dices;
                     converted_elements.push(converted_argument);
                 }
                 converted_formula = `[${converted_elements.join(',')}]`;
                 break;
             case mfb.Dice:
-                let [amount, amount_attribute_dependencies] = this.convert_formula(formula_block.amount);
-                let [magnitude, magnitude_attribute_dependencies] = this.convert_formula(formula_block.magnitude);
+                let [amount, amount_attribute_dependencies, amount_uses_dices] = this.convert_formula(formula_block.amount);
+                let [magnitude, magnitude_attribute_dependencies, magnitude_uses_dices] = this.convert_formula(formula_block.magnitude);
                 if (amount === null || magnitude === null) return [null, []];
                 for (let dependency of amount_attribute_dependencies) attribute_dependencies.add(dependency);
                 for (let dependency of magnitude_attribute_dependencies) attribute_dependencies.add(dependency);
+                use_dices = true;
+                // ^ amount_uses_dices & magnitude_uses_dices aren't important, because dice is already used here
                 converted_formula = `Array(${amount}).fill(undefined).map((value) => (dices_iterator.get(${magnitude}))).reduce((partialSum, a) => partialSum + a, 0)`;
                 break;
             default:
                 return [null, []];
         }
-        return [converted_formula, attribute_dependencies];
+        return [converted_formula, attribute_dependencies, use_dices];
     }
 }
 
